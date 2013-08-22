3e634ed68152bec0ad47f53b2f026d33f62f4f89
Nima Ehsani <nehsani00@ccny.cuny.edu>
2013-08-22 13:09:25 -0400

    printf

--- src/MDReservoirs.c
+++ src/MDReservoirs.c
@@ -330,7 +330,7 @@ static void _MDReservoirNeuralNet(int itemID) {
             SIMOUT = (resStorage - minresStorage + (discharge * 3600 * 24)) / (3600 * 24);
             if (SIMOUT < 0) {
                 printf("Error: Negative Discharge (3)! \n");
-                printf("%f \n", SIMOUT);
+                printf("%f %f %f %f \n", SIMOUT, resStorage, minresStorage, discharge);
             }
             resStorage = minresStorage;
         }
